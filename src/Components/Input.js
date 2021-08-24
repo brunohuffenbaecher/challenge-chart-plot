7c4b7b0d1c1a5a7ed2ebe861483be281cc7f62e7
brunohuffenbaecher <bruno.henrique56@hotmail.com>
2021-08-24 13:46:37 -0300

    Created the Input component and helper function to convert the code

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -1,5 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { Controlled as CodeMirror } from 'react-codemirror2';
+import { codeParse } from '../Helpers/codeConverter';
+
+import './input.module.css';
+
+require('codemirror/mode/javascript/javascript');
+require('codemirror/mode/css/css');
+require('codemirror/lib/codemirror.css');
+require('codemirror/theme/dracula.css');
+require('codemirror/theme/panda-syntax.css');
+require('codemirror/theme/material.css');
+
+//lines of code for input testing
+const initialCode = `{type: 'start',timestamp: 1519862400000,select: ['min_response_time', 'max_response_time'],group: ['os', 'browser']}
+{type: 'span', timestamp: 1519862400000, begin: 1519862460000, end: 1519862460000}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'stop', timestamp: 1519862460000}`;
+
+// const initialCode = `{"type": 'start', "timestamp": 1519862400000,"select": ['min_response_time', 'max_response_time'],"group": ['os', 'browser']}
+// {"type": 'span', "timestamp": 1519862400000, "begin": 1519862460000, "end": 1519862460000}
+// {"type": 'data', "timestamp": 1519862400000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862400000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862400000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862460000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862460000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862460000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'data', "timestamp": 1519862460000, "os": 'linux', "browser": 'chrome', "min_response_time": 0.1, "max_response_time": 1.3}
+// {"type": 'stop', "timestamp": 1519862460000}`;
+
+// prettier-ignore
+
+// const jsonTest = [{type: 'start',timestamp: 1519862400000,select: ['min_response_time', 'max_response_time'],group: ['os', 'browser']},
+// {type: 'span', timestamp: 1519862400000, begin: 1519862460000, end: 1519862460000},
+// {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3},
+// {type: 'stop', timestamp: 1519862460000}];
+
+// let test = JSON.stringify(jsonTest[0]);
+// test = test.slice(1, test.length - 1);
+// let test = '';
+
+// for (let i = 0; i < jsonTest.length; i++) {
+//   // console.log(JSON.stringify(jsonTest[i]));
+//   // test.push(JSON.stringify(jsonTest[i]));
+//   test = test + JSON.stringify(jsonTest[i]) + '\n';
+// }
+// console.log(test);
+
+// let modifiedCode =
+//   '[' +
+//   initialCode.split('\n').join(',').replaceAll(' ', '').replaceAll("'", '"') +
+//   ']';
+// console.log(modifiedCode);
+// console.log(JSON.stringify(jsonTest));
+// console.log(JSON.parse(modifiedCode));
+// console.log(test);
+// strToJSON(initialCode);
+// console.log(jsonTest);
 
 export default function Input() {
-  return <div>I'm the Input component</div>;
+  const [code, setCode] = useState(initialCode);
+  
+
+  const handleChange = (editor, data, value) => {
+    // console.log('Editor: ', editor);
+    // console.log('Data: ', data);
+    // console.log('Value: ', value);
+    setCode(value);
+
+    codeParse(value); // function to convert the input code
+    // console.log(value);
+  };
+
+  return (
+    <div>
+      <CodeMirror
+        
+        value={code}
+        options={{
+          lineNumbers: true,
+          foldGutter: false,
+          styleActiveLine: false,
+          autofocus: true,
+          theme: 'dracula',
+          mode: 'application/json',
+        }}
+        
+        onBeforeChange={handleChange}
+      />
+    </div>
+  );
 }
---
+++ src/Helpers/codeConverter.js
@@ -0,0 +1,26 @@
+import JSON5 from 'json5';
+
+export const strToJSON = (str) => {
+  let lines = str.split('\n');
+
+  for (let i = 0; i < lines.length; i++) {
+    lines[i] = lines[i].slice(1, lines[i].length - 1);
+    lines[i] = lines[i].split(':');
+  }
+
+  console.log(lines);
+};
+
+/**Converts the text input block into an object array using JSON5 library */
+export const codeParse = (textBlock) => {
+  let convertedCode = '[' + textBlock.split('\n').join(',') + ']';
+  // convertedCode = JSON5.parse(convertedCode);
+  try {
+    convertedCode = JSON5.parse(convertedCode);
+    console.log(convertedCode);
+    return convertedCode;
+  } catch (error) {
+    alert('Check your input code for errors!');
+    return;
+  }
+};
