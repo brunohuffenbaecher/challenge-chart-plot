5d66d4d46526e9b22998c47947e8bbf35d619c0e
brunohuffenbaecher <bruno.henrique56@hotmail.com>
2021-08-24 23:04:37 -0300

    Header component configured

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import css from './header.module.css';
 
 export default function Header() {
-  return <div>I'm the header component</div>;
+  return <div className={css.headerDiv}>Bruno Oliveira's Challenge</div>;
 }
